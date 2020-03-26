9231f6a0777345ba6dd035bad04cfdf9d1bd8f63
mar-y-m <mar.minaeva@gmail.com>
2020-03-26 14:06:08 +0100

    26.03

---
+++ src/lesson2603.java
@@ -0,0 +1,22 @@
+public class lesson2603 {
+    public static void main(String[] args){
+
+        String[] stringArray = new String[]{"1","254"};
+
+        int resultValue = 0;
+
+        for (int i = 0; i < stringArray.length; i++) {
+
+            String currentValueString = stringArray[i];
+
+
+            int currentValue = Integer.parseInt(currentValueString);
+            resultValue=resultValue+currentValue;
+        }
+
+        System.out.println(resultValue);
+
+
+
+    }
+}
